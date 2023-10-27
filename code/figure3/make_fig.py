cb9bd9c5f97acf6f01caf93f856ac1426bd513b4
Thomas Bury <tombury182@gmail.com>
2023-10-26 23:37:24 -0400

    code for fig  4

--- code/figure3/make_fig.py
+++ code/figure3/make_fig.py
@@ -28,96 +28,96 @@ import plotly.graph_objects as go
 
 
 
-# #------------
-# # Stacked plot for VV-VN linear regressions
-# #-------------
+#------------
+# Stacked plot for VV-VN linear regressions
+#-------------
 
-# # Import data
-# df_reg = pd.read_csv('../output/df_reg_vv_vn.csv')
+# Import data
+df_reg = pd.read_csv('../output/df_reg_vv_vn.csv')
 
-# # Assign color based on nn_mean
-# colors = px.colors.sample_colorscale('Turbo', 100, low=0.0, high=1.0, colortype='rgb')[::-1]
-# nn_min = df_reg['nn_mean'].min()
-# nn_max = df_reg['nn_mean'].max()+0.01
+# Assign color based on nn_mean
+colors = px.colors.sample_colorscale('Turbo', 100, low=0.0, high=1.0, colortype='rgb')[::-1]
+nn_min = df_reg['nn_mean'].min()
+nn_max = df_reg['nn_mean'].max()+0.01
 
-# def get_col(nn_mean):
-#     prop = (nn_mean-nn_min)/(nn_max-nn_min)
-#     assert prop>=0
-#     idx = int(100*prop)
-#     col = colors[idx]
-#     return col
+def get_col(nn_mean):
+    prop = (nn_mean-nn_min)/(nn_max-nn_min)
+    assert prop>=0
+    idx = int(100*prop)
+    col = colors[idx]
+    return col
 
-# df_reg['color'] = df_reg['nn_mean'].apply(get_col)
-
-
-# # color map based on nn_mean
-# color_discrete_map = dict()
-# for segment in df_reg['segment'].unique():
-#     col = df_reg[df_reg['segment']==segment]['color'].iloc[0]
-#     color_discrete_map[segment] = col
-
-# list_df = []
-# for segment in df_reg['segment'].unique():
-#     dic = df_reg.query('segment==@segment').iloc[0]
-#     xvals = np.linspace(dic['xmin'], dic['xmax'], 100)
-#     yvals = dic['b']*xvals + dic['c']
-#     df = pd.DataFrame({'x':xvals, 'y':yvals, 'b':dic['b'], 'c':dic['c'], 'nn_mean':dic['nn_mean'], 
-#                         'segment':dic['segment'], 'r':dic['r']})
-#     list_df.append(df)
+df_reg['color'] = df_reg['nn_mean'].apply(get_col)
 
 
-# df_plot = pd.concat(list_df)
+# color map based on nn_mean
+color_discrete_map = dict()
+for segment in df_reg['segment'].unique():
+    col = df_reg[df_reg['segment']==segment]['color'].iloc[0]
+    color_discrete_map[segment] = col
+
+list_df = []
+for segment in df_reg['segment'].unique():
+    dic = df_reg.query('segment==@segment').iloc[0]
+    xvals = np.linspace(dic['xmin'], dic['xmax'], 100)
+    yvals = dic['b']*xvals + dic['c']
+    df = pd.DataFrame({'x':xvals, 'y':yvals, 'b':dic['b'], 'c':dic['c'], 'nn_mean':dic['nn_mean'], 
+                        'segment':dic['segment'], 'r':dic['r']})
+    list_df.append(df)
 
 
-# fig = px.line(df_plot, x='x', y='y', color='segment',
-#               hover_data=['b','c','nn_mean','r'],
-#               color_discrete_map=color_discrete_map,
-#               )
-# fig.update_layout(showlegend=False)
-
-# fig.update_traces(line=dict(width=1))
-
-# fig.update_xaxes(title='VN (s)', 
-#                     range=[0.5,1.7],
-#                   )
-# fig.update_yaxes(title='VV (s)', 
-#                     range=[0.9,2.35],
-#                   )
+df_plot = pd.concat(list_df)
 
 
-# fig.update_layout(height=300,
-#                   width=300,
-#                   margin=dict(l=10, r=10, t=10, b=10)
-#                   )
+fig = px.line(df_plot, x='x', y='y', color='segment',
+              hover_data=['b','c','nn_mean','r'],
+              color_discrete_map=color_discrete_map,
+              )
+fig.update_layout(showlegend=False)
 
-# fig.write_image('../../results/figure3b.png', scale=2,)
+fig.update_traces(line=dict(width=1))
+
+fig.update_xaxes(title='VN (s)', 
+                    range=[0.5,1.7],
+                  )
+fig.update_yaxes(title='VV (s)', 
+                    range=[0.9,2.35],
+                  )
+
+
+fig.update_layout(height=300,
+                  width=300,
+                  margin=dict(l=10, r=10, t=10, b=10)
+                  )
+
+fig.write_image('../../results/figure3b.png', scale=2,)
 
 
 
-# #------------
-# # Make histogram of gradient values for VV-VN regression
-# #-------------
+#------------
+# Make histogram of gradient values for VV-VN regression
+#-------------
 
-# df_plot = df_reg.melt(id_vars='segment', 
-#                       value_vars=['b'],
-#                       var_name='feature', 
-#                       value_name='val',
-#                       )
+df_plot = df_reg.melt(id_vars='segment', 
+                      value_vars=['b'],
+                      var_name='feature', 
+                      value_name='val',
+                      )
 
-# fig = px.histogram(df_plot, x='val',
-#                     nbins=60,)
+fig = px.histogram(df_plot, x='val',
+                    nbins=60,)
 
-# fig.update_xaxes(matches=None)
-# fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
+fig.update_xaxes(matches=None)
+fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
 
-# fig.update_xaxes(title='slope')
-# fig.update_yaxes(title='count')
+fig.update_xaxes(title='slope')
+fig.update_yaxes(title='count')
 
-# fig.update_layout(height=200,
-#                   width=300,
-#                   margin=dict(l=10, r=10, t=10, b=10)
-#                   )
-# fig.write_image('../../results/figure3d.png', scale=2,)
+fig.update_layout(height=200,
+                  width=300,
+                  margin=dict(l=10, r=10, t=10, b=10)
+                  )
+fig.write_image('../../results/figure3d.png', scale=2,)
 
 
 
---
+++ code/figure4/make_fig.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 22:10:33 2023
+
+Make figure 4
+Histogram showing slope of linear regression for
+- patient data
+- simulation of reentry
+- simulation of reentry with conduction delay
+
+@author: tbury
+"""
+
+
+
+import numpy as np
+import pandas as pd
+import os
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+sample_rate = 180
+
+# Import data
+df = pd.read_csv('../output/df_vv_stats.csv')
+df_reg = pd.read_csv('../output/df_reg_vv_vn.csv')
+
+df_reg['unex_var'] = (1-df_reg['r']**2)*df_reg['total_variance']
+
+# Convert to s
+df['NN'] = df['NN']/sample_rate
+df['NV'] = df['NV']/sample_rate
+df['VN'] = df['VN']/sample_rate
+df['VV'] = df['VV']/sample_rate
+
+
+#----------------
+# Model for reentry - fixed CI with noise
+# VV = VN + t_c + eps
+#----------------
+
+# Get an approximation for t_c (conduction time)
+tc = (df['VV']-df['VN']).mean()
+
+# Get approximation for noise term
+sigma = np.sqrt(df_reg['unex_var'].mean())
+
+list_b_m1 = []
+list_b_patient = []
+list_c_m1 = []
+list_c_patient = []
+
+list_p_slopes_m1 = []
+list_p_intercept_m1 = []
+
+
+for segment in df_reg['segment'].unique():
+    
+    ## Patient data
+    df_patient = df.query('segment==@segment')
+    # Lin regression of segment
+    b_patient = df_reg.query('segment==@segment')['b'].iloc[0]
+    c_patient = df_reg.query('segment==@segment')['c'].iloc[0]
+    r_patient = df_reg.query('segment==@segment')['r'].iloc[0]
+    se_slope_patient = df_reg.query('segment==@segment')['se_slope'].iloc[0]
+    se_inter_patient = df_reg.query('segment==@segment')['se_intercept'].iloc[0]
+    total_variance = df_reg.query('segment==@segment')['total_variance'].iloc[0]
+    
+    
+    # Simulate model
+    vn_vals = df_patient['VN']
+    noise_vals = np.random.normal(0, sigma, len(vn_vals))
+    vv_vals = vn_vals + tc + noise_vals
+    df_null = pd.DataFrame({'VN':vn_vals, 'VV':vv_vals})
+    
+    # Compute linear regression 
+    out = stats.linregress(vn_vals, vv_vals)
+    b_null, c_null, r_null, p_null, se_slope_null = out
+    se_inter_null = out.intercept_stderr
+    reg_text_null = "y={}x+{},  R^2={}".format(round(b_null, 4), round(c_null, 4), round(r_null**2, 4))
+    # print(reg_text_null)
+    
+    list_b_m1.append(b_null)
+    list_c_m1.append(c_null)
+
+    list_b_patient.append(b_patient)
+    list_c_patient.append(c_patient)
+    
+
+#----------------
+# Model:
+# VV = b*VN + c + eps
+#----------------
+
+# Obtain b as mean slope of linear regressions
+b = df_reg['b'].mean()
+
+# Obtain c as mean intercept of linear regressions
+c = df_reg['c'].mean()
+
+# Get approximation for noise term
+sigma = np.sqrt(df_reg['unex_var'].mean())
+
+list_b_m2 = []
+list_b_patient = []
+list_c_m2 = []
+list_c_patient = []
+
+list_p_slopes_m2 = []
+list_p_intercept_m2 = []
+
+
+for segment in df_reg['segment'].unique():
+    
+    ## Patient data
+    df_patient = df.query('segment==@segment')
+    # Lin regression of segment
+    b_patient = df_reg.query('segment==@segment')['b'].iloc[0]
+    c_patient = df_reg.query('segment==@segment')['c'].iloc[0]
+    r_patient = df_reg.query('segment==@segment')['r'].iloc[0]
+    se_slope_patient = df_reg.query('segment==@segment')['se_slope'].iloc[0]
+    se_inter_patient = df_reg.query('segment==@segment')['se_intercept'].iloc[0]
+    total_variance = df_reg.query('segment==@segment')['total_variance'].iloc[0]
+    
+    # Simulate model
+    vn_vals = df_patient['VN']
+    noise_vals = np.random.normal(0, sigma, len(vn_vals))
+    vv_vals = b*vn_vals + c + noise_vals
+    df_null = pd.DataFrame({'VN':vn_vals, 'VV':vv_vals})
+    
+   
+    # Compute linear regression
+    out = stats.linregress(vn_vals, vv_vals)
+    b_null, c_null, r_null, p_null, se_slope_null = out
+    se_inter_null = out.intercept_stderr
+    reg_text_null = "y={}x+{},  R^2={}".format(round(b_null, 4), round(c_null, 4), round(r_null**2, 4))
+    # print(reg_text_null)
+    
+    list_b_m2.append(b_null)
+    list_c_m2.append(c_null)
+
+    list_b_patient.append(b_patient)
+    list_c_patient.append(c_patient)
+
+
+
+########### Plot to compare b values
+df_plot = pd.DataFrame(
+    {'reentry':list_b_m1,
+      'reentry+variable conduction':list_b_m2,
+      'patient data':list_b_patient
+      }
+    )
+
+df_plot = df_plot.melt(value_vars=['patient data','reentry', 'reentry+variable conduction'])
+
+fig = px.histogram(df_plot, x='value', color='variable', nbins=60)
+fig.update_layout(barmode='overlay')
+
+# Reduce opacity to see both histograms
+fig.update_traces(opacity=0.75)
+
+fig.update_layout(width=400,
+                  height=300,
+                  legend=dict(
+                      yanchor="top",
+                      y=0.99,
+                      xanchor="left",
+                      x=0.01,
+                      title='',
+                      ),
+                  margin=dict(l=0, r=10, t=10, b=10)
+                  )
+
+fig.update_xaxes(title='slope of linear regression')
+fig.update_yaxes(title='count')
+
+fig.write_image('../../results/figure4.png', scale=2)
+
+
+
+
+
+
+
+
+
+
